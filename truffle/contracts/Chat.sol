2fee9dea39d8797993f0c36345824f8495d25b95
P1F <rtsouza15@gmail.com>
2021-12-04 02:53:53 -0400

    added more things to chat contract

--- truffle/contracts/Chat.sol
+++ truffle/contracts/Chat.sol
@@ -3,34 +3,43 @@ pragma solidity ^0.8.0;
 
 contract Chat {
     uint256 public messagesCount = 0;
-    uint256 public usersCount = 0;
 
     struct User {
-        uint256 id;
         string name;
     }
 
     struct Message {
         uint256 id;
+        address sender;
         string content;
         uint256 date; // in Unix timestammp
     }
 
-    mapping(uint256 => User) public users;
+    mapping(address => User) public users;
     mapping(uint256 => Message) public messages;
 
-    constructor() {
-        createUser('user001');
-        createMessage('message001', 0);
+    function userExists(address pubkey) public view returns (bool) {
+        return bytes(users[pubkey].name).length > 0;
     }
 
     function createUser(string memory _name) public {
-        usersCount++;
-        users[usersCount] = User(usersCount, _name);
+        require(!userExists(msg.sender));
+        users[msg.sender] = User(_name);
+    }
+
+    function getUsername(address pubkey) public view returns (string memory) {
+        require(userExists(msg.sender));
+        return users[pubkey].name;
     }
 
     function createMessage(string memory _content, uint256 _date) public {
+        require(userExists(msg.sender));
         messagesCount++;
-        messages[messagesCount] = Message(messagesCount, _content, _date);
+        messages[messagesCount] = Message(
+            messagesCount,
+            msg.sender,
+            _content,
+            _date
+        );
     }
 }
